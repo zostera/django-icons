2c145ef99b8a88e825729bb8b8511fd6db75c69f
Marc Bourqui <m.bourqui@edsi-tech.com>
2018-06-25 00:17:07 +0200

    WIP: support arbitrary variants * refacto name parsing * refacto variants handling * rename methods, made classmethods when needed * more doc

--- django_icons/renderers/image.py
+++ django_icons/renderers/image.py
@@ -1,7 +1,10 @@
+import re
+
 from django.forms.utils import flatatt
 from django.templatetags.static import static
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 
 from django_icons.css import merge_css_text
 from django_icons.renderers.base import BaseRenderer
@@ -9,42 +12,76 @@ from django_icons.renderers.base import BaseRenderer
 
 class ImageRenderer(BaseRenderer):
     """
-    Render an icon from a local file.
+    Render an icon from a local file, each icon being a single file. The icon will be rendered using an <img> tag.
+
+    All the icons must have the same extensions. Variants of an icon can be saved under variants (color, size) of the
+    filename, following a pre-defined pattern (customizable).
+
+    For each rendered icon, the default CSS classes will be 'icon' and a composed name class 'icon-{name}' where
+    {name} is the name of the icon.
+    If color and/or size variants are defined, by default composed name classes will be added, following the
+    'icon-{variant}' pattern. There will be one class per variant.
+
+    By default, icons are assumed to be located in the 'static/icons' folder. But this can be customized, to have for
+    example one folder for each color and/or size.
+
+    For example, to support different colors of an icon, you could postfix the color code as '-c:w' for white or '-c:r'
+    for red. Or supporting different sizes like '-s:48' for 48 pixels or '-s:96' for 96 pixels.
 
     For example, calling in a template:
-        {% icons 'myicon' %}
+        {% icon 'myicon' %}
 
     will produce the following tag:
         <img src="/static/icons/myicon.png'" class="icon icon-myicon" %}">
 
+    To customize the default behaviours, create you own ImageRenderer by subclassing this one and override the methods
+    to suit you needs.
+
     """
 
-    def get_class(self):
-        """
-        First class, usually defines the icon
-        """
-        return 'icon icon-{name}'.format(name=self.name)
+    from collections import namedtuple
 
-    def get_icon_prefix(self):
+    VariantPattern = namedtuple('VariantPattern', ['key', 'pattern'])
+    _variant_patterns = dict()
+
+    def __init__(self, *args, **kwargs):
+        super(ImageRenderer, self).__init__(*args, **kwargs)
+        # 'alt' is a mandatory tag attribute
+        self.kwargs.setdefault('alt', _('{} icon'.format(self.name.title())))
+        self.variant = dict()
+
+    @classmethod
+    def get_image_root(cls):
+        """
+        The root path to the images folder. By default, returns the path to a 'icons' folder inside the static folder.
+
+        Returns
+        -------
+        str or callable
+
+        """
+        return static('icons')
+
+    @classmethod
+    def get_image_prefix(cls):
         """
         Filename prefix. For example, Icons8 icons are all prefixed with `icons8-`, so that you can call your icons
         without repeating the prefix in the name.
         """
         return ''
 
-    def get_icons_dirname(self):
+    @classmethod
+    def get_image_variant_patterns(cls):
         """
-        Relative path to the icons folder, starting from the static folder. Do not include the trailing '/'.
-        """
-        return 'icons'
 
-    def get_size(self):
-        """
-        Icon size, in case several sizes are available (assumed postfixed with `-XX` where XX is the size)
-        """
-        return None
+        Returns
+        -------
 
-    def get_format(self):
+        """
+        return [cls.VariantPattern('color', '-c:(?P<{}>\w+)'), cls.VariantPattern('size', '-s:(?P<{}>\w+)')]
+
+    @classmethod
+    def get_image_format(cls):
         """
         Icon format, without dot
 
@@ -52,16 +89,79 @@ class ImageRenderer(BaseRenderer):
         """
         return 'png'
 
+    @classmethod
+    def _get_image_v_p_regex(cls):
+        """
+        Compile the variant regular expression patterns into a regular expression object
+
+        Returns
+        -------
+        Regular expression object
+
+        """
+
+        if not cls._variant_patterns:
+            for v in cls.get_image_variant_patterns():
+                cls._variant_patterns[v.key] = re.compile(v.pattern.format(v.key))
+        return cls._variant_patterns
+
+    def get_variant(self):
+        """
+
+        Returns
+        -------
+        dict : dict containing the variant values
+
+        """
+        if not self.variant:
+            for key, pattern in self._get_image_v_p_regex().items():
+                variant = pattern.search(self.name)
+                if variant:
+                    self.variant[key] = variant.group(key)
+                    self.name = pattern.sub('', self.name)
+        return self.variant
+
+    def render_variant(self):
+        """
+
+        Returns
+        -------
+        str : The string representing the variant to be appended to the icon name to build the path to the file in the
+                file system
+
+        """
+        variant = self.get_variant()
+        v_s = ''
+        if variant:
+            for v in self.get_image_variant_patterns():
+                if v.key in variant:
+                    v_s += '-{}'.format(variant[v.key])
+        return v_s
+
     def get_path(self):
         """
-        Relative path to the icon
+        Relative path to the icon.
+
+        By default, the icon filename is built as '{name}[-{color}][-{size}][-{variantX}]' where '-{color}' '-{size}'
+        and '-{variantX}'s are only added if there are defined (if several are defined, they are added in that order).
         """
-        dirname = self.get_icons_dirname()
-        filename = self.get_icon_prefix() + self.name
-        if self.get_size():
-            filename += '-' + str(self.get_size())
-        filename += '.' + self.get_format()
-        return static('{}/{}'.format(dirname, filename))
+        filename = self.get_image_prefix() + self.name + self.render_variant() + '.' + self.get_image_format()
+        return '{}/{}'.format(self.get_image_root(), filename)
+
+    def get_class(self):
+        """
+        First CSS classes: 'icon' and 'icon-{name}' where {name} is the given name of the icon in the templatetag.
+
+        If the icon defines a color and/or size, 'icon-{color}' and/or 'icon-{size}' classes will be added as well.
+        """
+        css_classes = 'icon'
+        if self.get_image_prefix():
+            css_classes += ' icon-{prefix}'.format(prefix=self.name)
+        for v_p in self.get_image_variant_patterns():
+            if v_p.key in self.get_variant():
+                css_classes += ' icon-{variant}-{value}'.format(variant=v_p.key, value=self.get_variant()[v_p.key])
+        css_classes += ' icon-{name}'.format(name=self.name)
+        return css_classes
 
     def render(self):
         """
--- tests/test_image.py
+++ tests/test_image.py
@@ -13,6 +13,26 @@ class ImageTest(TestCase):
 
     def test_icons(self):
         self.assertEqual(
-            render_template('{% icon "icons8-icons8-48" renderer="ImageRenderer" %}'),
             '<img src="/static/icons/icons8-icons8-48.png" class="icon icon-icons8-icons8-48">',
+            render_template('{% icon "icons8-icons8-48" renderer="ImageRenderer" %}'),
+        )
+        self.assertEqual(
+            '<img src="/static/icons/icons8-48.png" class="icon icon-icons8-48">',
+            render_template('{% icon "icons8-48" renderer="ImageRenderer" %}'),
+        )
+        self.assertEqual(
+            '<img src="/static/icons/icons8-48.png" class="icon icon-size-48 icon-icons8">',
+            render_template('{% icon "icons8-s:48" renderer="ImageRenderer" %}'),
+        )
+        self.assertEqual(
+            '<img src="/static/icons/icons8-b.png" class="icon icon-color-b icon-icons8">',
+            render_template('{% icon "icons8-c:b" renderer="ImageRenderer" %}'),
+        )
+        self.assertEqual(
+            '<img src="/static/icons/icons8-b-48.png" class="icon icon-color-b icon-size-48 icon-icons8">',
+            render_template('{% icon "icons8-c:b-s:48" renderer="ImageRenderer" %}'),
+        )
+        self.assertEqual(
+            '<img src="/static/icons/icons8-b-48.png" class="icon icon-color-b icon-size-48 icon-icons8">',
+            render_template('{% icon "icons8-s:48-c:b" renderer="ImageRenderer" %}'),
         )
