6500a91331171c75413028addc237775c8bb0a23
Dylan Verheul <dylan@dyve.net>
2022-08-08 08:12:14 +0200

    Release 22.1 (#241)
    * Update CHANGELOG
    * Bump version to 22.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name="django-icons",
     zip_safe=False,
-    version="21.3",
+    version="22.1",
     description="Icons for Django",
     long_description=README,
     long_description_content_type="text/markdown",
